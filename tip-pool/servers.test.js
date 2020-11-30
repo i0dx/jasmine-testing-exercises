6c115a4d6eb1888b56f45e918f28742b46563bfa
Mateo Segura <musegura21@gmail.com>
2020-11-30 17:20:41 -0500

    Completed servers.js testing [Step 1]

--- tip-pool/servers.test.js
+++ tip-pool/servers.test.js
@@ -1,17 +1,33 @@
 describe("Servers test (with setup and tear-down)", function() {
-  beforeEach(function () {
-    // initialization logic
-    serverNameInput.value = 'Alice';
-  });
+    beforeEach(function() {
+        // initialization logic
+        serverNameInput.value = 'Alice';
+    });
 
-  it('should add a new server to allServers on submitServerInfo()', function () {
-    submitServerInfo();
+    it('should add a new server to allServers on submitServerInfo()', function() {
+        submitServerInfo();
 
-    expect(Object.keys(allServers).length).toEqual(1);
-    expect(allServers['server' + serverId].serverName).toEqual('Alice');
-  });
+        expect(Object.keys(allServers).length).toEqual(1);
+        expect(allServers['server' + serverId].serverName).toEqual('Alice');
+    });
 
-  afterEach(function() {
-    // teardown logic
-  });
+    it('should properly enter a new server into the table', function() {
+        submitServerInfo();
+        updateServerTable();
+
+        const serverCheck = document.querySelectorAll('#serverTable tbody tr td');
+        expect(serverCheck.length).toEqual(2);
+        expect(serverCheck[0].innerText).toEqual('Alice');
+        expect(serverCheck[1].innerText).toEqual('$0.00');
+    })
+
+    afterEach(function() {
+        // teardown logic
+        allServers = {};
+        serverId = 0;
+        const cleanUp = document.querySelector("#serverTable tbody").childNodes
+        cleanUp.forEach(function(key) {
+            key.remove();
+        })
+    });
 });
